be9fd77abebad5f629c623f745676056d49f0bd6
oskery <oskar.lyrstrand@gmail.com>
2020-09-30 12:49:14 +0200

    Add example text

--- script.js
+++ script.js
@@ -5,5 +5,5 @@ function traverseBackground () {
   // bg.style.left = -Math.min(window.scrollY, 2489-window.innerWidth) + 'px'
 
   // Change style of background. Specifically change left-attribute to minus the amount of scroll
-  bg.style.left = -window.scrollY + 'px'
+  bg.style.backgroundPositionX = -window.scrollY + 'px'
 }
