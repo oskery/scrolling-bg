5a61963503364737804ed20ee6673a6c9d68d576
oskery <oskar.lyrstrand@gmail.com>
2020-09-30 12:53:35 +0200

    Remove hardcoded width of image and cleanuo

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 const bg = document.getElementById("bg");
-// bg.style.left
 
 function traverseBackground () {
-  // bg.style.left = -Math.min(window.scrollY, 2489-window.innerWidth) + 'px'
-
-  // Change style of background. Specifically change left-attribute to minus the amount of scroll
   bg.style.backgroundPositionX = -window.scrollY + 'px'
 }
